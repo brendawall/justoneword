2012b6a611ca09ec1ec1eb53f5a5df2328263265
Blake Wollston <theweeklyjubs@gmail.com>
2023-05-20 19:26:10 +0100

    Added Animation

--- script.js
+++ script.js
@@ -10,6 +10,60 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function formatNumber(number, length) {
+	return number.toString().padStart(length, '0') + '.png';
+  }
+  
+  function animateVideoSequence(containerElement, sequenceArray, frameDelay) {
+	let imagesLoaded = 0;
+	let totalImages = 0;
+  
+	sequenceArray.forEach(sequence => {
+	  let delay = frameDelay; // Initial delay duration
+  
+	  for (let i = 1; i <= sequence.length; i++) {
+		if (sequence.type === 'logo') {
+		  const image = new Image();
+		  image.src = `${sequence.path}/${formatNumber(i, 4)}`;
+		  image.onload = () => {
+			imagesLoaded++;
+  
+			if (imagesLoaded === totalImages) {
+			  startAnimation(); // Start the animation once all images are loaded
+			}
+		  };
+  
+		  totalImages++;
+		}
+	  }
+	});
+  
+	function startAnimation() {
+	  let delay = frameDelay; // Initial delay duration
+  
+	  sequenceArray.forEach(sequence => {
+		for (let i = 1; i <= sequence.length; i++) {
+			setTimeout(() => {
+			  containerElement.style.background = `url(${sequence.path}/${formatNumber(i, 4)}) no-repeat center`;
+			  containerElement.style.backgroundSize = `156%`;
+			}, delay);
+  
+			delay += frameDelay; // Increment the delay for each frame
+		}
+	  });
+	}
+  }
+  
+  // Example usage
+  const logoContainer = document.querySelector('.logo');
+  const sequenceArray = [
+	{ type: 'logo', path: './ink-overlays/logo/Logo1/image-sequence', length: 297 }
+  ];
+  const frameDelay = 50;
+  
+  animateVideoSequence(logoContainer, sequenceArray, frameDelay);
+  
   
   
   
