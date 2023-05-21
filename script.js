7206e8e36f2aad7bfdf413067155d54903e247e3
InterPixel <theweeklyjubs@gmail.com>
2023-05-21 18:52:14 +0100

    Added much simpler video masking technique and delays

--- script.js
+++ script.js
@@ -11,66 +11,11 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-function formatNumber(number, length) {
-	return number.toString().padStart(length, '0') + '.png';
-  }
-  
-  function animateVideoSequence(containerElement, sequenceArray, frameDelay) {
-	let imagesLoaded = 0;
-	let totalImages = 0;
-  
-	sequenceArray.forEach(sequence => {
-	  let delay = frameDelay; // Initial delay duration
-  
-	  for (let i = 1; i <= sequence.length; i++) {
-		if (sequence.type === 'logo') {
-		  const image = new Image();
-		  image.src = `${sequence.path}/${formatNumber(i, 4)}`;
-		  image.onload = () => {
-			imagesLoaded++;
-  
-			if (imagesLoaded === totalImages) {
-			  startAnimation(); // Start the animation once all images are loaded
-			}
-		  };
-  
-		  totalImages++;
-		}
-	  }
-	});
-  
-	function startAnimation() {
-	  let delay = frameDelay; // Initial delay duration
-	  let zoomLength = 10;
-  
-	  sequenceArray.forEach(sequence => {
-		for (let i = 1; i <= sequence.length; i++) {
-			setTimeout(() => {
-			  if(sequence.inverted) {containerElement.style.filter = 'invert()'}
-			  containerElement.style.background = `url(${sequence.path}/${formatNumber(i, 4)}) no-repeat center`;
-			  if(sequence.center) {
-				if(i <= zoomLength) {
-					containerElement.style.backgroundSize = `${i * (156 / zoomLength)}%`
-				} else {
-				containerElement.style.backgroundSize = `156%`;
-				};	
-			  }	  
-			}, delay);
-  
-			delay += frameDelay; // Increment the delay for each frame
-		}
-	  });
-	}
-  }
-  
-  // Example usage
-  const logoContainer = document.querySelector('.logo');
-  const sequenceArray = [
-	{ type: 'logo', path: './ink-overlays/logo/Logo1/image-sequence', length: 297, inverted: false, center: true }
-  ];
-  const frameDelay = 40;
-  
-  animateVideoSequence(logoContainer, sequenceArray, frameDelay);
+const logo = document.querySelector('.logo');
+logo.playbackRate = 2;
+setTimeout(() => {
+	logo.classList.add('animation-end')
+}, 4000);
   
   function typeWriter(text, speed, pause, random) {
 	const originalText = text.textContent;
@@ -81,21 +26,23 @@ function formatNumber(number, length) {
 	
 	text.textContent = ''
 
-	for(let n = 0; n<modifiedText.length; n++) {
-		if(pauseArray.includes(modifiedText[n])) {
-			let randomSpeedItem = Math.floor(Math.random() * random * speed + pause);
-			sum += randomSpeedItem;
-			randomSpeedArray.push(sum)
-		} else {
-			let randomSpeedItem = Math.floor(Math.random() * random * speed);
-			sum += randomSpeedItem;
-			randomSpeedArray.push(sum)
+	setTimeout(() => {
+		for(let n = 0; n<modifiedText.length; n++) {
+			if(pauseArray.includes(modifiedText[n])) {
+				let randomSpeedItem = Math.floor(Math.random() * random * speed + pause);
+				sum += randomSpeedItem;
+				randomSpeedArray.push(sum)
+			} else {
+				let randomSpeedItem = Math.floor(Math.random() * random * speed);
+				sum += randomSpeedItem;
+				randomSpeedArray.push(sum)
+			}
+	
+			setTimeout(() => {
+				text.textContent += modifiedText[n]
+			}, randomSpeedArray[n-1]);		
 		}
-
-		setTimeout(() => {
-			text.textContent += modifiedText[n]
-		}, randomSpeedArray[n-1]);		
-	}
+	}, 5000);
   }
 
   const paragraph = document.querySelector('p');
